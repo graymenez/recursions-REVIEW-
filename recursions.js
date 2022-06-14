c93db74ea29f2d9981639602031fdf5f1e33464f
gray jimenez <coreyjimenez1@gmail.com>
2022-06-13 22:59:23 -0600

    first commit

---
+++ recursions.js
@@ -0,0 +1,72 @@
+// ************************************************************************************************
+// ********************                                      **************************************
+// ********************                                      **************************************
+// ******************** Definetly need to review all of this **************************************
+// ********************                                      **************************************
+// ********************                                      **************************************
+// ************************************************************************************************
+
+/** product: calculate the product of an array of numbers. */
+
+function product(nums, idx = 0) {
+  if (idx === nums.length) return 1;
+  return nums[idx] * product(nums, idx + 1);
+}
+
+/** longest: return the length of the longest word in an array of words. */
+
+function longest(words, idx = 0, longestSoFar = 0) {
+  if (idx === words.length) return longestSoFar;
+  longestSoFar = Math.max(words[idx].length, longestSoFar);
+  return longest(words, idx + 1, longestSoFar);
+}
+
+/** everyOther: return a string with every other letter. */
+
+function everyOther(str, idx = 0, newStr = "") {
+  if (idx >= str.length) return newStr;
+  newStr += str[idx];
+  return everyOther(str, idx + 2, newStr);
+}
+
+/** isPalindrome: checks whether a string is a palindrome or not. */
+
+function isPalindrome(str) {}
+
+/** findIndex: return the index of val in arr (or -1 if val is not present). */
+
+function findIndex(arr, val) {}
+
+/** revString: return a copy of a string, but in reverse. */
+
+function revString(str, idx = 0, newStr = "") {
+  if (idx >= str.length) return newStr;
+  newStr += str[idx];
+  if (idx === str.length - 1) {
+    return newStr
+      .split("")
+      .reverse()
+      .reduce((a, b) => a.concat(b));
+  }
+  return revString(str, idx + 1, newStr);
+}
+
+/** gatherStrings: given an object, return an array of all of the string values. */
+
+function gatherStrings(obj) {}
+
+/** binarySearch: given a sorted array of numbers, and a value,
+ * return the index of that value (or -1 if val is not present). */
+
+function binarySearch(arr, val) {}
+
+module.exports = {
+  product,
+  longest,
+  everyOther,
+  isPalindrome,
+  findIndex,
+  revString,
+  gatherStrings,
+  binarySearch,
+};
